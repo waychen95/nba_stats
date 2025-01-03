e0807e0c8d621a356f4d9274ad85fc82649e4e7e
waychen95 <wchen206@my.bcit.ca>
2025-01-02 20:26:16 -0800

    fixed playerlist page bug

--- frontend/src/pages/PlayerList.jsx
+++ frontend/src/pages/PlayerList.jsx
@@ -151,7 +151,7 @@ function PlayerList() {
         </div>
       ) : (
         <>
-          <p className='updated-time'>Last Updated: 2024/12/17</p>
+          <p className='updated-time'>Last Updated: 2024/01/02</p>
           <ul className='player-list'>
             {players.map((player) => (
               <Link to={`/players/${player.id}`} key={player.id} className='player-card'>
